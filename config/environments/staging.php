1832593502d83eadfc874c596b3fc966b5ffd4bc
DanielHirunrusme <daniel@halfhalf.us>
2017-10-02 09:47:09 -0500

    removed old wp

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -8,7 +8,7 @@ define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
 define('WP_HOME', getenv('WP_HOME'));
 define('WP_SITEURL', getenv('WP_SITEURL'));
 
-ini_set('display_errors', 0);
-define('WP_DEBUG_DISPLAY', false);
-define('SCRIPT_DEBUG', false);
+//ini_set('display_errors', 0);
+//define('WP_DEBUG_DISPLAY', false);
+//define('SCRIPT_DEBUG', false);
 //define( 'WP_DEBUG', true );
